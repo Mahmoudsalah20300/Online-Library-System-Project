cd43e511bc639e0328cf8b4dbda7131c8b4de8f4
Mahmoud Salah <Mahmoudsalah20300@gmail.com>
2023-12-25 16:23:20 +0200

    The class that contains Booklist

--- Online Library System Project/User_list.cpp
+++ Online Library System Project/User_list.cpp
@@ -1,2 +1,56 @@
-
 #include "User_list.h"
+
+
+
+UserList::UserList()
+{
+	capacity = 0;
+	usersCount = 0;
+	users = nullptr;
+}
+
+UserList::UserList(int capacity)
+{
+	this->capacity = capacity;
+	users = new User[capacity];
+	usersCount = 0;
+}
+
+
+void UserList::addUser(User& user)
+{
+	if (usersCount < capacity) {
+		users[usersCount] = user;
+		usersCount++;
+	}
+	else
+		cout << "Segmentation Error: You surpassed array Capacity" << endl;
+}
+
+User* UserList::searchUser(string name)
+{
+	for (int i = 0; i < usersCount; i++)
+	{
+		if (users[i].getName() == name)
+		{
+			return &users[i];
+		}
+	}
+	return nullptr;
+}
+
+
+
+User* UserList::searchUser(int id)
+{
+	for (int i = 0; i < usersCount; i++)
+	{
+		if (users[i].getId() == id)
+		{
+			return &users[i];
+		}
+	}
+	return nullptr;
+}
+
+
